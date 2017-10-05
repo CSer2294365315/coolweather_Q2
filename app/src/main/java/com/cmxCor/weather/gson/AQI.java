7034fd0c3ae4ac8569e9c27bd4bc3279851160f0
Q2294365315 <2294365315@qq.com>
2017-10-06 01:42:18 +0800

    well

---
+++ app/src/main/java/com/cmxCor/weather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.cmxCor.weather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/cmxCor/weather/gson/Basic.java
@@ -0,0 +1,22 @@
+package com.cmxCor.weather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update {
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+
+}
